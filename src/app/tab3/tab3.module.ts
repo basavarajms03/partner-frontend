312466f83411706dfb6c3486c47f69a44e34e18f
Basavaraj Mallappa <basavaraj@amazecodes.local>
2020-06-12 09:19:19 +0530

    Notification added to the vendor application

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 import { MoreInfoComponent } from './more-info/more-info.component';
 import { EstimationComponent } from './estimation/estimation.component';
+import { NotifcationComponent } from '../tabs/notifcation/notifcation.component';
 
 @NgModule({
   imports: [
@@ -16,9 +17,10 @@ import { EstimationComponent } from './estimation/estimation.component';
     RouterModule.forChild([
       { path: '', component: Tab3Page },
       { path: 'more-info', component: MoreInfoComponent },
-      { path: 'estimate', component: EstimationComponent }
+      { path: 'estimate', component: EstimationComponent },
+      { path: 'notification', component: NotifcationComponent }
     ])
   ],
-  declarations: [Tab3Page, MoreInfoComponent, EstimationComponent]
+  declarations: [Tab3Page, MoreInfoComponent, EstimationComponent, NotifcationComponent]
 })
 export class Tab3PageModule { }
---
+++ src/app/tabs/notifcation/notifcation.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifcation',
+  templateUrl: './notifcation.component.html',
+  styleUrls: ['./notifcation.component.scss'],
+})
+export class NotifcationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
