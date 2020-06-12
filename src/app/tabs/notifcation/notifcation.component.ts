4d4929c50a38fc12733d4e65f484ac344a966862
Basavaraj Mallappa <basavaraj@amazecodes.local>
2020-06-12 23:36:32 +0530

    Notification is added

--- src/app/tabs/notifcation/notifcation.component.ts
+++ src/app/tabs/notifcation/notifcation.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/shared/api.service';
+import { LoadingComponent } from 'src/app/shared/loading/loading.component';
+import { charSet } from "./notification.constants";
 
 @Component({
   selector: 'app-notifcation',
   templateUrl: './notifcation.component.html',
   styleUrls: ['./notifcation.component.scss'],
+  providers: [LoadingComponent]
 })
 export class NotifcationComponent implements OnInit {
+  notifications: any;
+  background = "navy";
+  code;
 
-  constructor() { }
+  constructor(private apiService: ApiService, private loading: LoadingComponent) { }
 
-  ngOnInit() {}
+  async ngOnInit() {
+
+    let loading = await this.loading.loading();
+    loading.present();
+
+    let notificationParams = {
+      "filter": {
+        "email": {
+          "$in": [localStorage.getItem('email')]
+        }
+      }
+    };
+
+    this.apiService.post('/v1/dashboard/getall-notification', notificationParams).subscribe(res => {
+      loading.dismiss();
+      this.code = charSet;
+      this.notifications = res['data'];
+    });
+
+  }
 
 }
