9e4ac61fba290ecd22689f0c2c7afd56d9fd8117
Basavaraj Mallappa <basavaraj.mallappa@amazecodes.com>
2020-10-14 10:10:52 +0530

    OTP files created

---
+++ src/app/authentication/verify-otp/verify-otp.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-otp',
+  templateUrl: './verify-otp.component.html',
+  styleUrls: ['./verify-otp.component.scss'],
+})
+export class VerifyOTPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
